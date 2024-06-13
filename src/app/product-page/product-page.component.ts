d1bdd3dced1e8100e9a06cba77285a77ce90f149
ZY324 <emma93324>
2024-06-13 21:36:37 +0800

    feat(product): 查詢列表單設定

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,5 +1,6 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subject, startWith, switchMap } from 'rxjs';
 import { Product } from '../model/product';
@@ -9,13 +10,19 @@ import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [AsyncPipe, ProductCardListComponent],
+  imports: [AsyncPipe, ReactiveFormsModule, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.css',
 })
 export class ProductPageComponent {
   //公告資料變動，用在新增、刪除
   private readonly refresh$ = new Subject<void>();
+
+  //表單資料
+  protected readonly formControl = new FormControl<string | undefined>(
+    undefined,
+    { nonNullable: true }
+  );
 
   //DI 依賴注入
   private productService = inject(ProductService);
